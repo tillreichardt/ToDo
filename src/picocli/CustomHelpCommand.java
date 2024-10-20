6193bbbc7d4dd98fc3a492e1ec7cb513f922d545
Till Reichardt <tillroger2006@gmail.com>
2024-10-21 00:01:49 +0200

    updated CustomHelp

--- src/picocli/CustomHelpCommand.java
+++ src/picocli/CustomHelpCommand.java
@@ -4,8 +4,8 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Model.OptionSpec;
 
-import java.util.List;
 
+import com.List;
 import com.cliNavigation;
 import com.Connector.DatabaseConnector;
 
@@ -32,7 +32,6 @@ public class CustomHelpCommand implements Runnable {
         CommandLine showCmd = new CommandLine(new ShowCommand());
         CommandLine createCmd = new CommandLine(new CreateCommand());
 
-        System.out.println("\nSort options:");
         printOptions(showCmd);
         System.out.println("\nCreateCommand options:");
         printOptions(createCmd);
@@ -40,29 +39,92 @@ public class CustomHelpCommand implements Runnable {
     
     private void printOptions(CommandLine commandLine, String... optionNames) {
         CommandSpec commandSpec = commandLine.getCommandSpec();
-        List<OptionSpec> options = commandSpec.options();
+        List<OptionSpec> options = new List<>();
     
-        for (OptionSpec option : options) {
-            String[] description = option.description();
-            String desc = description.length > 0 ? description[0] : "";  // Falls keine Beschreibung vorhanden ist
-    
-            // Typ der Option (Klassenname des Parameters)
-            String paramType = option.type().getSimpleName();
-    
-            // Falls optionNames angegeben wurden, nur diese Optionen zeigen
-            if (optionNames.length == 0 || arrayContains(optionNames, option.longestName())) {
+        // add options
+        for (OptionSpec option : commandSpec.options()) {
+            options.append(option);
+        }
+
+        List<OptionSpec> userOptions = new List<>();
+        List<OptionSpec> todoOptions = new List<>();
+        List<OptionSpec> categoryOptions = new List<>();
+        List<OptionSpec> sortOptions = new List<>();
+        
+        // sort options into these 4 lists
+        options.toFirst(); 
+        while (options.hasAccess()) {
+            OptionSpec option = options.getContent();
+            // options for ShowCommand
+            if (commandLine.getCommandName().equals("show") && (option.longestName().contains("sort-by") || option.longestName().contains("order"))) {
+                sortOptions.append(option);
+            } 
+            // user options (username, password)
+            else if (option.longestName().contains("username") || option.longestName().contains("password")) {
+                userOptions.append(option);
+            } 
+            // ToDo options (title, description, priority, category)
+            else if (option.longestName().contains("title") || option.longestName().contains("description") || option.longestName().contains("priority") || option.longestName().contains("category")) {
+                todoOptions.append(option); 
+
+                // add to category if it is the title
+                if (option.longestName().contains("title")) {
+                    categoryOptions.append(option); 
+                }
+            }
+            options.next();
+        }
+        
+        // show sort options
+        if (!sortOptions.isEmpty()) {
+            System.out.println("\nSort options:");
+            sortOptions.toFirst();
+            while (sortOptions.hasAccess()) {
+                OptionSpec option = sortOptions.getContent();
+                String paramType = option.type().getSimpleName();
+                String desc = option.description().length > 0 ? option.description()[0] : ""; // use desc or "" if there is non
                 System.out.printf("  %-15s %-15s %s%n", option.longestName(), "<" + paramType + ">", desc);
+                sortOptions.next();
             }
         }
-    }
     
-    
-    private boolean arrayContains(String[] array, String value) {
-        for (String item : array) {
-            if (item.equals(value)) {
-                return true;
+        // show user options
+        if (!userOptions.isEmpty()) {
+            System.out.println("  user:");
+            userOptions.toFirst(); 
+            while (userOptions.hasAccess()) {
+                OptionSpec option = userOptions.getContent();
+                String paramType = option.type().getSimpleName();
+                String desc = option.description().length > 0 ? option.description()[0] : ""; // use desc or "" if there is non
+                System.out.printf("    %-15s %-15s %s%n", option.longestName(), "<" + paramType + ">", desc); 
+                userOptions.next();
             }
         }
-        return false;
+    
+        // show todo options
+        if (!todoOptions.isEmpty()) {
+            System.out.println("  todo:");
+            todoOptions.toFirst(); 
+            while (todoOptions.hasAccess()) {
+                OptionSpec option = todoOptions.getContent();
+                String paramType = option.type().getSimpleName();
+                String desc = option.description().length > 0 ? option.description()[0] : ""; // use desc or "" if there is non
+                System.out.printf("    %-15s %-15s %s%n", option.longestName(), "<" + paramType + ">", desc);
+                todoOptions.next();
+            }
+        }
+    
+        // show category options
+        if (!categoryOptions.isEmpty()) {
+            System.out.println("  category:");
+            categoryOptions.toFirst();
+            while (categoryOptions.hasAccess()) {
+                OptionSpec option = categoryOptions.getContent();
+                String paramType = option.type().getSimpleName();
+                String desc = option.description().length > 0 ? option.description()[0] : ""; // use desc or "" if there is non
+                System.out.printf("    %-15s %-15s %s%n", option.longestName(), "<" + paramType + ">", desc);
+                categoryOptions.next(); 
+            }
+        }
     }
 }
